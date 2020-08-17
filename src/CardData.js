ac0749239ec251f22999ad39089c86bb70b8c2b4
Beka <bekamakharoblishvili@gmail.com>
2020-08-17 13:28:32 +0400

    Change Title, Made Card component and card data

---
+++ src/CardData.js
@@ -0,0 +1,11 @@
+const cardData = {
+  name: 'Beka',
+  url: 'https://www.linkedin.com/in/beka-makharoblishvili/',
+  headline:
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+  message:
+    'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form',
+  urldisplayname: 'https://www.linkedin.com/in/beka....',
+};
+
+export default cardData;
